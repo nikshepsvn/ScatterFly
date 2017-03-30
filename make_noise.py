20a0948bee578d279591c0dd3e3268b87010a1cc
nikshepsvn <nikshepsvn@gmail.com>
2017-03-30 13:59:05 -0400

    completed v1

---
+++ make_noise.py
@@ -0,0 +1,24 @@
+#necassary imports
+import os
+import platform
+import time
+from random import randint
+from selenium import webdriver
+
+#setting up driver to simulate user and also start chrome in background
+from selenium.webdriver.chrome.options import Options
+chrome_options = Options()
+chrome_options.add_argument("--no-startup-window")
+
+#choosing chrome driver based on OS
+if 'Linux' in (platform.system()):
+    driver = webdriver.Chrome(os.getcwd()+'/chromedriver_linux',chrome_options=chrome_options)
+elif 'Windows' in (platform.system()):
+    driver = webdriver.Chrome(os.getcwd()+'/chromedriver.exe',chrome_options=chrome_options)
+elif 'Darwin' in (platform.system()):
+    driver = webdriver.Chrome(os.getcwd()+'/chromedriver_mac',chrome_options=chrome_options)
+
+#indefinte loop to visit random website and stay on the page for a random amount of time
+while(1):
+    driver.get("http://www.uroulette.com/visit/oqroon");
+    time.sleep(randint(0,7))
