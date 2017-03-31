c594818de7d498d004dce97a4fdfe7fc4f54dac2
nikshepsvn <nikshepsvn@gmail.com>
2017-03-31 17:39:35 -0400

    added option to create fake data in popular sites -- right now reddit is implemented

--- make_noise.py
+++ make_noise.py
@@ -21,9 +21,35 @@ elif 'Windows' in (platform.system()):
 elif 'Darwin' in (platform.system()):
     driver = webdriver.Chrome(os.getcwd()+'/chromedriver_mac',chrome_options=chrome_options)
 
+linklist = []
 #indefinte loop to visit random website and stay on the page for a random amount of time
+print '''Please select which of these sites you visit most often (choose all that is applicable)
+1. Reddit
+2. Facebook
+3. YouTube
+4. Tumblr
+5. Amazon
+6. Ebay'''
+
 
 while(1):
-    driver.get("http://www.uroulette.com/visit/oqroon")
-    print "currently on site:" + driver.current_url
-    time.sleep(randint(0,7))
+    x = raw_input()
+    if (x != "S"):
+        linklist.append(x)
+    else:
+        print "You have succesfully entered " + (str(len(linklist))) + " sites."
+        break;
+
+def randomsite():
+        driver.get("http://www.uroulette.com/visit/oqvsoq")
+        time.sleep(randint(0,7))
+        print "currently on site: " + driver.current_url
+
+def randomreddit():
+    driver.get("http://reddit.com/r/random")
+    time.sleep(randint(5,20))
+    print "currently on site: " + driver.current_url
+
+while(1):
+    if '1' in linklist:
+        randomreddit()
