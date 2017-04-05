a6b4a13c9e47651fa585a78f88bdbe24521bb7a1
nikshepsvn <nikshepsvn@gmail.com>
2017-04-05 18:07:45 -0400

    did some basic code clean up and modularization

--- make_noise.py
+++ make_noise.py
@@ -7,76 +7,81 @@ import time
 from random import choice, randint
 from selenium import webdriver
 
-#initializing drivers
-print("Starting drivers ... ")
 
-# checking if user is attempting to run pynoise on a RPi, and if so, initializing different drivers.
-if 'raspberrypi' in platform.uname() or 'armv7l' == platform.machine():
-    if not os.getenv('DISPLAY'):
-        print("make sure to start a virtual display:")
-        print("Xvfb :99 -ac &")
-        print("export DISPLAY=:99")
-        sys.exit(1)
+with open(os.getcwd()+'/data/nounlist.txt') as f:
+    words = f.read().splitlines()
 
-    #adding user agent and headless argument to browser
-    from selenium.webdriver.firefox.options import Options
-    firefox_options = Options()
-    firefox_options.add_argument("--headless")
-    firefox_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36")
+def init_drivers():
+    #initializing drivers
+    print("Starting drivers ... ")
 
-    #initializing driver
-    p = os.getcwd() + '/geckodriver_arm7'
-    driver = webdriver.Firefox(executable_path=p, firefox_options=firefox_options)
+    # checking if user is attempting to run pynoise on a RPi, and if so, initializing different drivers.
+    if 'raspberrypi' in platform.uname() or 'armv7l' == platform.machine():
+        if not os.getenv('DISPLAY'):
+            print("make sure to start a virtual display:")
+            print("Xvfb :99 -ac &")
+            print("export DISPLAY=:99")
+            sys.exit(1)
 
-else:
-    #adding user agent and headless argument to browser
-    from selenium.webdriver.chrome.options import Options
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36")
+        #adding user agent and headless argument to browser
+        from selenium.webdriver.firefox.options import Options
+        firefox_options = Options()
+        firefox_options.add_argument("--headless")
+        firefox_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36")
 
-    #choosing chrome driver based on OS and initializing it for further use
-    if 'Linux' in (platform.system()):
-        driver = webdriver.Chrome(os.getcwd()+'/chromedriver_linux',chrome_options=chrome_options)
-    elif 'Windows' in (platform.system()):
-        driver = webdriver.Chrome(os.getcwd()+'/chromedriver.exe',chrome_options=chrome_options)
-    elif 'Darwin' in (platform.system()):
-        driver = webdriver.Chrome(os.getcwd()+'/chromedriver_mac',chrome_options=chrome_options)
+        #initializing driver
+        p = os.getcwd() + '/drivers/geckodriver_arm7'
+        return webdriver.Firefox(executable_path=p, firefox_options=firefox_options)
 
-
-#starting pynoise..
-print "PyNoise is now going to start generating some random traffic."
-
-#asking user for input on which sites users browse to improve pynoise's ability to contaminate the data
-print '''Please select which of these sites you visit most often (choose all that is applicable) (input S when you're finished):
-1. Reddit
-2. Facebook
-3. YouTube
-4. Tumblr
-5. Amazon
-6. Ebay'''
-
-#creating an AL link user input and fucntions
-sites_dict = {
-    '0': 'randomsite()',
-    '1': 'randomreddit()',
-    '2': 'random_fb()',
-    '3': 'random_youtube()',
-    '4': 'random_tumblr()',
-    '5': 'random_amazon()',
-    '6': 'random_ebay()'
-}
-
-#loop to input the data
-# start with randomsite as default
-linklist = ['0']
-while(1):
-    x = raw_input()
-    if (x != "S"):
-        linklist.append(x)
     else:
-        print "You have succesfully entered " + (str(len(linklist)-1)) + " sites."
-        break;
+        #adding user agent and headless argument to browser
+        from selenium.webdriver.chrome.options import Options
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36")
+
+        #choosing chrome driver based on OS and initializing it for further use
+        if 'Linux' in (platform.system()):
+            return webdriver.Chrome(os.getcwd()+'/drivers/chromedriver_linux',chrome_options=chrome_options)
+        elif 'Windows' in (platform.system()):
+            return webdriver.Chrome(os.getcwd()+'/drivers/chromedriver.exe',chrome_options=chrome_options)
+        elif 'Darwin' in (platform.system()):
+            return webdriver.Chrome(os.getcwd()+'/drivers/chromedriver_mac',chrome_options=chrome_options)
+
+
+def get_input():
+    #asking user for input on which sites users browse to improve pynoise's ability to contaminate the data
+    print '''Please select which of these sites you visit most often (choose all that is applicable) (input S when you're finished):
+    1. Reddit
+    2. Facebook
+    3. YouTube
+    4. Tumblr
+    5. Amazon
+    6. Ebay'''
+
+    #creating an AL link user input and fucntions
+    sites_dict = {
+        '0': 'randomsite()',
+        '1': 'randomreddit()',
+        '2': 'random_fb()',
+        '3': 'random_youtube()',
+        '4': 'random_tumblr()',
+        '5': 'random_amazon()',
+        '6': 'random_ebay()'
+    }
+
+    #loop to input the data
+    # start with randomsite as default
+    linklist = ['0']
+    while(1):
+        x = raw_input()
+        if (x != "S"):
+            linklist.append(x)
+        else:
+            print "You have succesfully entered " + (str(len(linklist)-1)) + " sites."
+            break;
+
+    return linklist, sites_dict
 
 #function to visit random webpages on the internet
 def randomsite():
@@ -118,7 +123,15 @@ def random_ebay():
     element.click();
     time.sleep(randint(0,7))
     print "currently on site: " + driver.current_url
-# loop to start the functions and visits
-while(1):
-    rnd_site = choice(linklist)
-    eval(sites_dict[rnd_site])
+
+def start_noise():
+    # loop to start the functions and visits
+    while(1):
+        rnd_site = choice(linklist)
+        eval(sites_dict[rnd_site])
+
+if __name__ == "__main__":
+    driver = init_drivers()
+    linklist, sites_dict= get_input()
+    print "ScatterFly is now going to start generating some random traffic."
+    start_noise()
