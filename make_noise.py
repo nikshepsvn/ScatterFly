3160cd791ab21e4c6d40c076906723ac8e5429dd
nikshepsvn <nikshepsvn@gmail.com>
2017-04-11 22:45:20 -0400

    minor bug fixes and made terminal output cleaner

--- make_noise.py
+++ make_noise.py
@@ -23,18 +23,18 @@ def get_random_word():
 
 #function to check and initialize drivers based on system
 def start_drivers():
-    platform = platform.system()
-    print("Attempting to initialize drivers....")
+    sysplatform = platform.system()
+    print("\nAttempting to initialize drivers....")
     #if user is using RPi, make sure that the virtual display has been setup or else exit
 
     if 'raspberrypi' in platform.uname() or 'armv7l' == platform.machine():
         #if user is running on raspberrypi and hasnt set up xvfb properly print instruction on how to set up and exit code
         if not os.getenv('DISPLAY'):
-            print("Please make sure that your virtual display is setup correctly and try again!")
-            print("Make sure you have executed the following commands: ")
-            print("1. xvfb :99 -ac &")
-            print("2. export DISPLAY=:99")
-            print("Now exiting Program...")
+            print("\nPlease make sure that your virtual display is setup correctly and try again!")
+            print("\nMake sure you have executed the following commands: ")
+            print("\n1. xvfb :99 -ac &")
+            print("\n2. export DISPLAY=:99")
+            print("\nNow exiting Program...")
             sys.exit(1)
 
         #adding options to firefox driver
@@ -45,8 +45,8 @@ def start_drivers():
         firefox_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36")
         #initializing driver with options
         p = currentpath + '/drivers/geckodriver_arm7'
+        print("\nDrivers for RaspberryPi has been initialized succesfully!")
         return webdriver.Firefox(executable_path = p, firefox_options = firefox_options)
-        print("Drivers for RaspberryPi has been initialized succesfully!")
 
     else: #enters here if device is not a RPi
         #creating a chrome options object that is later going to be attached with the driver!
@@ -56,35 +56,35 @@ def start_drivers():
         chrome_options.add_argument("--mute-audio")
         chrome_options.add_argument("user-agent=Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36")
         #choosing and initializing driver based on OS
-        if 'Linux' in (platform):
+        if 'Linux' in (sysplatform):
+            print("\nDrivers for Linux has been initialized succesfully!")
             return webdriver.Chrome(currentpath +'/drivers/chromedriver_linux',chrome_options = chrome_options)
-            print("Drivers for Linux has been initialized succesfully!")
-        elif 'Windows' in (platform):
+        elif 'Windows' in (sysplatform):
+            print("\nDrivers for Windows has been initialized succesfully!")
             return webdriver.Chrome(currentpath +'/drivers/chromedriver.exe',chrome_options = chrome_options)
-            print("Drivers for Windows has been initialized succesfully!")
-        elif 'Darwin' in (platform):
+        elif 'Darwin' in (sysplatform):
+            print("\nDrivers for OSX has been initialized succesfully!")
             return webdriver.Chrome(currentpath +'/drivers/chromedriver_mac',chrome_options = chrome_options)
-            print("Drivers for OSX has been initialized succesfully!")
 
 
 def request_user_data():
-    print("ScatterFly can use your previous data to make its ability to obfuscate and generate noise more accurate.")
-    print("This requires you to give ScatterFly permission to access your previous browsing history.")
-    print("Please keep in mind ScatterFly is extremely secure and will not comprimise this data in anyway or form since this data will never leave your machine.")
+    print("\nScatterFly can use your previous data to make its ability to obfuscate and generate noise more accurate.")
+    print("\nThis requires you to give ScatterFly permission to access your previous browsing history.")
+    print("\nPlease keep in mind ScatterFly is extremely secure and will not comprimise this data in anyway or form since this data will never leave your machine.")
     permission = input("ScatterFly is requesting permission to acess user data, type 'Yes' to grant permissions or 'No' to deny permission.")
 
-    if permission == "Yes" or permission == "yes" or permission ="y" or permission =="Y":
-        print("ScatterFly will now analyze your data to make it's obfuscation more intelligent")
+    if permission == "Yes" or permission == "yes" or permission =="y" or permission =="Y":
+        print("\nScatterFly will now analyze your data to make it's obfuscation more intelligent")
         activity_data = process_data()
         obfuscate(activity_data)
     else:
-        print("ScatterFly has been denied permission to access data, ScatterFly will continue running.")
+        print("\nScatterFly has been denied permission to access data, ScatterFly will continue running.")
         obfuscate("empty")
 
 #get_input is a function gets information from the user
 def get_input():
     #asking user for input on which sites users browse to improve ScatterFly's ability to contaminate the data
-    print '''Please select which of these sites you visit most often (choose all that is applicable) (input S when you're finished):
+    print '''\nPlease select which of these sites you visit most often \n(choose all that is applicable and input S when finished):
     1. Reddit
     2. YouTube
     3. Tumblr
@@ -208,7 +208,7 @@ def start_noise(linklist, sites_dict):
 
 #main method
 if __name__ == "__main__":
-    driver = init_drivers()
+    driver = start_drivers()
     linklist, sites_dict = get_input()
     print "ScatterFly is now going to start generating some random traffic."
     start_noise(linklist, sites_dict)
